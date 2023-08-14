6d38a95ac117e197724d065349b2cdb4e10ceae9
Sergey Gromov <looneyxx@yandex.ru>
2023-08-14 14:52:12 +0300

    добавил env

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+import dotaenv from 'dotenv';
+
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -9,6 +11,8 @@ import router from './routes/index.js';
 import errorHandler from './middlewares/error.js';
 import { PORT, BASE_URL } from './utils/config.js';
 
+dotaenv.config();
+console.log(process.env);
 const app = express();
 app.use(helmet());
 app.use(cookieParser());
