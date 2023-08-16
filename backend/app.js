6c6996339507665a1f23be7c3d2024638841d48d
Sergey Gromov <looneyxx@yandex.ru>
2023-08-16 15:28:13 +0300

    TEST

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
@@ -9,11 +9,10 @@ import { errors } from 'celebrate';
 import router from './routes/index.js';
 import errorHandler from './middlewares/error.js';
 import { DB_CONN, PORT } from './utils/config.js';
-
 const app = express();
 app.use(helmet());
 app.use(cookieParser());
-dotenv.config();
+
 app.disable('x-powered-by');
 app.use(cors({ origin: 'https://cardsplace.nomoreparties.co', credentials: true }));
 app.use(bodyParser.json());
